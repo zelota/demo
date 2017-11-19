5bcc7b1f35df87406c58846e77e4ed5516ddf91f
peter.varsanyi <varp@freemail.hu>
2017-11-19 15:37:36 +0100

    init

---
+++ src/main/java/proba/demo/DemoApplication.java
@@ -0,0 +1,18 @@
+package proba.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * http://www.baeldung.com/bootstraping-a-web-application-with-spring-and-java-based-configuration
+ *
+ * http://www.baeldung.com/the-persistence-layer-with-spring-data-jpa
+ *
+ */
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+}
